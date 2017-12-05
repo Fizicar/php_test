3f04529b1d9c42773f6786163289fcda81750ec8
lukazivanovic <sdluka@hotmail.com>
2017-12-05 16:56:25 +0100

    prvi commit

---
+++ index.php
@@ -0,0 +1,218 @@
+<?php 
+//require_once('api.php');
+//require_once('admin.php');
+//require_once('login.php');
+//require_once('register.php');
+//require_once('includes/connection.php');
+//require_once('includes/functions.php');
+?>
+<?php
+
+?>
+
+<html>
+<head>
+  <title>PetsCare</title>
+  <script src="https://use.fontawesome.com/ffb6023ab4.js"></script>
+  <link href="assets/css/styles.css" media="all" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+</head>
+<body>
+<nav class="navbar navbar-toggleable-md" id="navbar1">
+    <div class="container">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleContainer" aria-controls="navbarsExampleContainer" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Welcome to the Best Pets Care at Melbourne</a>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleContainer">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+        <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+        <a href=""><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+        <a href=""><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+        <a href=""><i class="fa fa-skype" aria-hidden="true"></i></a>
+        </form>
+    </div>
+    </div>
+</nav>
+    <nav class="navbar navbar-toggleable-md" id="navbar2">
+      <div class="container">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleContainer" aria-controls="navbarsExampleContainer" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="https://placeholder.com"><img src="http://via.placeholder.com/100x50"></a>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleContainer">
+          <ul class="navbar-nav mr-auto">
+          </ul>
+        </div>
+      </div>
+</nav>
+    <nav class="navbar navbar-toggleable-md" id="navbar3">
+      <div class="container">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleContainer" aria-controls="navbarsExampleContainer" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExampleContainer">
+          <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">HOME</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown02">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ABOUT US</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SERVICES</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown02">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+              </li>
+              <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PAGES</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown02">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">GALLERY</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">BLOG</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown02">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SHOP</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown02">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#">CONTACTS</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-md-0" style="color:black;">
+          Open Hours: Mon-Fri: 09:00am - 20:00pm
+          </form>
+        </div>
+      </div>
+    </nav>
+
+
+
+    <section class="bigPicture">
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>We Provide the Best Care for Your Pets</h1> 
+    <p>The best pets clinic at Melbourne, more than 20.000+ customers happy.</p> 
+    <p><a class="btn btn-md" href="#" role="button">More about us</a></p>
+  </div>
+</div>            
+</section>
+
+<section class="welcome">
+    <div class="container">
+    <div class="row">
+    <div class="col">
+    <img src="http://via.placeholder.com/400x400">
+</div>
+<div class="col">
+    <div class="main_service_content">
+    <h1>Welcome to Pets Cate</h1>
+    <h2>Your pet's health and well-being are out top priority.</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore omnis voluptas beatae eligendi quisquam, molestiae consectetur ab magnam sed voluptatem!</p>
+    <div class="row">                            
+    <div class="col-5">
+                                    <div class="single_service">
+                                        <div class="single_service_icon">
+                                            <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="single_service_content">
+                                            <h3>Pet Grooming</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quisquam adipisci
+                                                suscipit vero. Quam, dolorem!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-5">
+                                    <div class="single_service">
+                                        <div class="single_service_icon">
+                                            <i class="fa fa-line-chart" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="single_service_content">
+                                            <h3>Pet Hotel</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quisquam adipisci
+                                                suscipit vero. Quam, dolorem!</p>
+                                        </div>
+                                    </div>
+                                </div>
+</div>
+<div class="row">
+                                <div class="col-5">
+                                    <div class="single_service">
+                                        <div class="single_service_icon">
+                                            <i class="fa fa-desktop" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="single_service_content">
+                                            <h3>Vaccination</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quisquam adipisci
+                                                suscipit vero. Quam, dolorem!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="border_divider"></div>
+                                <div class="col-5">
+                                    <div class="single_service">
+                                        <div class="single_service_icon">
+                                            <i class="fa fa-book" aria-hidden="true"></i>
+                                        </div>
+                                        <div class="single_service_content">
+                                            <h3>Pet Care</h3>
+                                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus eum deserunt
+                                                quidem provident error voluptatum.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div></div>
+                        </div>
+                    </div>
+                </div>
+
+</div>
+</div>
+</section>
+<?php
+
+?>
+</div>
+
+
+</div>
+<div id="footer">
+</div>
+<script src="assets/javascript/main.js"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php if(isset($database)) { $database->close_connection(); } ?>
